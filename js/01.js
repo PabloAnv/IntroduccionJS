b32dd86fa3683d086b059196a7e02cba99d046a1
PabloAnv <pabloprogramador0799@gmail.com>
2023-03-10 19:37:48 -0600

    Variables

--- js/01.js
+++ js/01.js
@@ -3,3 +3,22 @@
 var producto = "Audifonos Gamer"; //Iniciar variable y declarar valor
 
 var disponible; //Iniciar variable
+
+producto = true; //Reasignando valor (no hay problema)
+
+disponible = true; //Declarando un valor
+
+var producto1 = "Computadora",
+    disponible1 = true,
+    categoria = "Computadoras";
+    //Se pueden declarar varias variables en una sola linea de codigo usando una coma y al final un punto y coma
+
+// var 1disponible; //No se pueden iniciar variables con un numero primero ni con guion a escepcion de guion bajo (como en todos los lenguajes)
+
+var disponible_; //Esto es correcto
+
+var nombre_producto = "Monitor HD"; //Esto tambien uderscore
+var nombreProducto = "Monito HD"; //cameCase
+var NombreProducto = "Monitor"; //Para clases PascalCase
+
+console.log(producto);

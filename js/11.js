97abb41763b125afb3c4d2f9604a7b9fe0200922
PabloAnv <pabloprogramador0799@gmail.com>
2023-03-12 16:24:43 -0600

    Modificar Objetos

--- js/11.js
+++ js/11.js
@@ -1,11 +1,19 @@
 //Objetos
-
+//Objetos
 const producto = {
     nombreProducto: 'Monitor de 20"',
     precio: 300,
     disponible: true
 }
 
+// console.log(producto);
+// console.log(producto.precio);
+// console.log(producto["precio"]);
+
+//Agregar propiedad al objeto
+producto.imagen = 'imagen.jpg';
+
+//Eliminar propiedad al objeto
+delete producto.disponible;
+
 console.log(producto);
-console.log(producto.precio);
-console.log(producto["precio"]);

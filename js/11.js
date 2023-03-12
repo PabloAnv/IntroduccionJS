56e5604967ac1f1276d28a7ce685a33eda3d9043
PabloAnv <pabloprogramador0799@gmail.com>
2023-03-12 15:23:55 -0600

    Objetos

---
+++ js/11.js
@@ -0,0 +1,11 @@
+//Objetos
+
+const producto = {
+    nombreProducto: 'Monitor de 20"',
+    precio: 300,
+    disponible: true
+}
+
+console.log(producto);
+console.log(producto.precio);
+console.log(producto["precio"]);

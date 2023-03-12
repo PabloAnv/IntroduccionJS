db03537d93969a5a14631dae5d073a22459fd967
PabloAnv <pabloprogramador0799@gmail.com>
2023-03-12 13:25:40 -0600

    Operadores y numeros

---
+++ js/06.js
@@ -0,0 +1,21 @@
+
+const numero1 = 100; //Numero
+const numero2 = 200; //Numero
+const numero3 = 200.20; //Numero flotante pero aqui no importa declararlo como flotante es decir decimal
+const numero4 = 0.1020; //Numero Flotante
+const numero5 = -5;     //Numero negativo
+const numero6 = 8;
+const numero7 = 6;
+const numeroFake ='300';  //String 
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+console.log(numero5);
+console.log(numero1 + numero2); //Operacion suma
+console.log(numero1 - numero2); //Operacion resta
+console.log(numero1 * numero2); //Operacion multiplicacion
+console.log(numero6 / numero7); //Operacion division
+console.log(numero6 % numero7); //Operacion residuo de la division, Modulo
+console.log(numeroFake); //String que fingue ser un numero

badd59ab2721fac8b20804a91247cbfc9b64fad4
PabloAnv <pabloprogramador0799@gmail.com>
2023-03-12 15:08:11 -0600

    Template Strings

---
+++ js/09.js
@@ -0,0 +1,8 @@
+const nombre = 'Pablo';
+const email = 'pablo@gmail.com'
+
+console.log('Nombre del cliente: ' + nombre + ' Correo del cliente: ' + email);
+
+//Template Strings o Strings Literals
+
+console.log(`Nombre del cliente: ${nombre} Correo del cliente: ${email}`);

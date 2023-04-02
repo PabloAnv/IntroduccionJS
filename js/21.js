54aff0b57f9ebfdd013c89004fd6798e92c7651f
PabloAnv <pabloprogramador0799@gmail.com>
2023-04-02 16:52:45 -0600

    Parametros y argumentos de una funcion

---
+++ js/21.js
@@ -0,0 +1,20 @@
+//Declaracion de una funcion
+
+//Parametros por default
+function sumar(numero1 = 0,numero2 = 0){ //Parametros de una funcion
+    console.log(numero1 + numero2);
+}
+
+sumar(5,3); //Argumentos de una funcion
+sumar(10,72);
+sumar(1); //Parametros por dafult
+sumar(); //Parametros por default
+
+//Expresion de la funcion
+
+
+const sumar2 = function (){
+    console.log (3 + 3);
+}
+
+sumar2();

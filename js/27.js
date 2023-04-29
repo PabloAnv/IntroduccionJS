c832e0da01cdf4b202851947532a4b2980d391c0
PabloAnv <pabloprogramador0799@gmail.com>
2023-04-29 13:50:29 -0600

    While y do While

--- js/27.js
+++ js/27.js
@@ -1,4 +1,4 @@
- //For lop
+//For lop
 
 //  for( let i = 0; i <= 10; i++ ){
 //     console.log(i);
@@ -12,23 +12,47 @@
 //     }
 // }
 
-const carrito = [
-    { nombreProducto: 'Monitor de 20"', precio: 300 },
-    { nombreProducto: 'Television de 50"', precio: 700 },
-    { nombreProducto: 'Tablet', precio: 300 },
-    { nombreProducto: 'Audifonos', precio: 200 },
-    { nombreProducto: 'Teclado', precio: 50 },
-    { nombreProducto: 'Celular', precio: 500 },
-    { nombreProducto: 'Bocinas', precio: 300 },
-    { nombreProducto: 'Laptop', precio: 800 }
-]
+// const carrito = [
+//     { nombreProducto: 'Monitor de 20"', precio: 300 },
+//     { nombreProducto: 'Television de 50"', precio: 700 },
+//     { nombreProducto: 'Tablet', precio: 300 },
+//     { nombreProducto: 'Audifonos', precio: 200 },
+//     { nombreProducto: 'Teclado', precio: 50 },
+//     { nombreProducto: 'Celular', precio: 500 },
+//     { nombreProducto: 'Bocinas', precio: 300 },
+//     { nombreProducto: 'Laptop', precio: 800 }
+// ]
 
-for( let i = 0; i < carrito.length; i++ ){
-    console.log(carrito[i].nombreProducto);
-}
+// for (let i = 0; i < carrito.length; i++) {
+//     console.log(carrito[i].nombreProducto);
+// }
 
- //While Loop
+//While Loop
+
+//  let i = 0; //Indice
+//  while( i<= 10){ //Condicion
+//     if (i % 2 ===0){
+//         console.log(`El numero ${i} es par`);
+//     }else{
+//         console.log(`El numero ${i} es impar`);
+//     }
+//     i++
+//  }
+
+// let i = 0;
+// while (i < carrito.length) {
+//     console.log(carrito[i].nombreProducto);
+//     i++
+// }
 
 
 
  //Do While Loop
+
+ let i = 100;
+ do{                //El do se cumplira primero almenos una vez aunque la condicion no se cumpla 
+    console.log(i);
+    i++
+ }
+    while(i <=10){}
+ 

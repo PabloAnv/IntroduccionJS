09f840b882a3923932c8995a54fd6982cad25287
PabloAnv <pabloprogramador0799@gmail.com>
2023-04-29 13:34:35 -0600

    foor loops

---
+++ js/27.js
@@ -0,0 +1,34 @@
+ //For lop
+
+//  for( let i = 0; i <= 10; i++ ){
+//     console.log(i);
+//  }
+
+// for( i = 1; i <= 100; i++){
+//     if(  i % 2 === 0 ){
+//         console.log(`${i} es un numero par`);
+//     }else{
+//         console.log(`${i} es un numero impar`);
+//     }
+// }
+
+const carrito = [
+    { nombreProducto: 'Monitor de 20"', precio: 300 },
+    { nombreProducto: 'Television de 50"', precio: 700 },
+    { nombreProducto: 'Tablet', precio: 300 },
+    { nombreProducto: 'Audifonos', precio: 200 },
+    { nombreProducto: 'Teclado', precio: 50 },
+    { nombreProducto: 'Celular', precio: 500 },
+    { nombreProducto: 'Bocinas', precio: 300 },
+    { nombreProducto: 'Laptop', precio: 800 }
+]
+
+for( let i = 0; i < carrito.length; i++ ){
+    console.log(carrito[i].nombreProducto);
+}
+
+ //While Loop
+
+
+
+ //Do While Loop

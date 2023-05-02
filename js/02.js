3ec4f8b210c5e34c39d56ecc52e82afbe17fd1d9
PabloAnv <pabloprogramador0799@gmail.com>
2023-05-01 19:50:20 -0600

    const y let

---
+++ js/02.js
@@ -0,0 +1,28 @@
+//Variables con let
+
+
+let producto = "Audionos Gamer";
+
+
+let disponible;     
+disponible = true;  
+
+
+let producto1 = "Mouse Gamer",
+    disponible1 = "True",
+    categoria = "Mouses";
+
+
+let nombre_producto = "Consola";
+
+let nombreProducto = "Consola";
+
+let NombreProducto = "Consola";
+
+let nombreproducto = "Consola";
+
+console.log(NOMBREPRODUCTO);
+
+
+
+
---
+++ js/03.js
@@ -0,0 +1,25 @@
+//Variables con const
+
+
+const producto = "Audionos Gamer";
+
+
+const disponible;     //Con const no se puede iniciar una variable sin declarar
+disponible = true;  
+
+
+const producto1 = "Mouse Gamer",
+    disponible1 = "True",
+    categoria = "Mouses";
+
+producto1 = "Pepe";     //No se puede reasignar un valor a la variable con const
+
+const nombre_producto = "Consola";
+
+const nombreProducto = "Consola";
+
+const NombreProducto = "Consola";
+
+const nombreproducto = "Consola";
+
+console.log(NOMBREPRODUCTO);

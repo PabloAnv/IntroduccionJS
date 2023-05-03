8ebc58386c7e4ef7600dba4d422acfdf48969b9c
PabloAnv <pabloprogramador0799@gmail.com>
2023-05-02 19:59:52 -0600

    Numeros

---
+++ js/05.js
@@ -0,0 +1,33 @@
+//Numeros
+
+const nombre = 'Pablo Alejandro Calvillo Ortega'
+
+//Los numeros se mostraran de color azul en la consola mientras que el string sera de color negro
+//Se pueden almacenar todo tipo de numeros
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 200.20;
+const numero4 = .1020;
+const numero5 = -5;
+
+console.log(nombre);
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+console.log(numero5);
+
+//Suma
+console.log(numero1 + numero2);
+
+//Resta
+console.log(numero1 - numero2);
+
+//Multiplicacion
+console.log(numero1 * numero2);
+
+//Division
+console.log(numero1 / numero2);
+
+//Residuo
+console.log(numero2 % numero1);

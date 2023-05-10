3368fa31bddecf32b4fef905fd5f859ea3952615
PabloAnv <pabloprogramador0799@gmail.com>
2023-05-09 20:18:20 -0600

    For loops

---
+++ js/24.js
@@ -0,0 +1,26 @@
+//For loops
+
+for (let i= 1; i <= 100; i++){
+    if( i % 2 === 0){
+        console.log(`El numero ${i} es par`);
+    }else{
+        console.log(`El numero ${i} es impar`);
+    }
+}
+
+
+const carrito = [
+    { nombre: "Monitor de 20\"", precio: 500 },
+    { nombre: "Television de 50\"", precio: 700 },
+    { nombre: "Tablet", precio: 300 },
+    { nombre: "Audifonos", precio: 200 },
+    { nombre: "Teclado", precio: 200 },
+    { nombre: "Celular", precio: 500 },
+    { nombre: "Bocinas", precio: 300 },
+    { nombre: "Laptop", precio: 800 },
+];
+
+//I es el indice
+for (let i=0; i < carrito.length; i++ ){
+    console.log(carrito[i]);
+}

78d2940cc350a76a4afaf623feeca65b368a1079
PabloAnv <pabloprogramador0799@gmail.com>
2023-05-15 19:28:07 -0600

    Promises

---
+++ js/30.js
@@ -0,0 +1,19 @@
+//Promises
+
+const usuarioAutenticado = new Promise((resolve, reject) => {
+    const auth = false;
+    if (auth) {
+        resolve('Usuario autenticado')   //El promise se cumple
+    } else {
+        reject('No se pudo iniciar sesion')    //El promise no se cumple
+    }
+});
+
+usuarioAutenticado
+    .then(resultado => console.log(resultado))
+    .catch(error => console.log(error))
+
+//En los promises se ocupan 3 valores
+//Pending: Que es pendiente, no se ha rechazado ni se a aceptado
+//Fulfilled: Ya se cumplio
+//Rejected: Se ha rechazado o no se puede cumplir
